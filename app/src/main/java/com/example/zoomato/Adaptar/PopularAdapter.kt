d0d555dbd051a3017deddd4c4a2b76358b55851c
pankaj rana <92663550+aspirers01@users.noreply.github.com>
2023-11-03 10:15:02 +0530

    home

---
+++ app/src/main/java/com/example/zoomato/Adaptar/PopularAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.zoomato.Adaptar
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.zoomato.databinding.ItemListBinding
+
+class PopularAdapter:RecyclerView.Adapter<PopularAdapter.PopularViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PopularViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    class PopularViewHolder(private val binding:ItemListBinding):RecyclerView.ViewHolder(binding.root) {
+
+    }
+}
--- app/src/main/java/com/example/zoomato/Fragment/Home_Fragment.kt
+++ app/src/main/java/com/example/zoomato/Fragment/Home_Fragment.kt
@@ -5,6 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView.ScaleType
+import android.widget.Toast
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.zoomato.R
 import com.example.zoomato.databinding.FragmentHomeBinding
 
@@ -16,8 +22,33 @@ class Home_Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_, container, false)
+        binding=FragmentHomeBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val imageList = ArrayList<SlideModel>() // Create image list
 
+// imageList.add(SlideModel("String Url" or R.drawable)
+// imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
+        imageList.add(SlideModel(R.drawable.d1))
+        imageList.add(SlideModel("https://bit.ly/2YoJ77H",ScaleTypes.FIT))
+        imageList.add(SlideModel("https://bit.ly/2BteuF2" ,ScaleTypes.FIT))
+        imageList.add(SlideModel("https://bit.ly/3fLJf72",ScaleTypes.FIT), )
+
+        val imageSlider = binding.imageSlider
+        imageSlider.setImageList(imageList)
+          imageSlider.setItemClickListener(object: ItemClickListener{
+              override fun doubleClick(position: Int) {
+                  TODO("Not yet implemented")
+              }
+
+              override fun onItemSelected(position: Int) {
+
+                  val itemmessage="select image $position"
+                  Toast.makeText(requireContext(), itemmessage, Toast.LENGTH_SHORT).show()
+              }
+          })
+    }
 }
