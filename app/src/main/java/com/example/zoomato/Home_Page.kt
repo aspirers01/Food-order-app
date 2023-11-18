cbfad0de2e900565299a783f4f7e39647506f016
pankaj rana <92663550+aspirers01@users.noreply.github.com>
2023-11-19 01:28:17 +0530

    impliments ui user profile

--- app/src/main/java/com/example/zoomato/Home_Page.kt
+++ app/src/main/java/com/example/zoomato/Home_Page.kt
@@ -1,20 +1,33 @@
 package com.example.zoomato
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.zoomato.databinding.ActivityHomePageBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Home_Page : AppCompatActivity() {
+    private lateinit var binding:ActivityHomePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
-
-
+        binding= ActivityHomePageBinding.inflate(layoutInflater)
+           setContentView(binding.root)
+        //
+        //settup for bottom navigation
         var NavController=findNavController(R.id.fragmentContainerView)
         var bottomnav=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomnav.setupWithNavController(NavController)
+
+        //setup to notification button
+          binding.notification.setOnClickListener{
+              val bottomfragment=Notifcation_BottomFragment()
+                bottomfragment.show(supportFragmentManager,"test string")
+          }
+
+
     }
 }
--- app/src/main/java/com/example/zoomato/Notifcation_BottomFragment.kt
+++ app/src/main/java/com/example/zoomato/Notifcation_BottomFragment.kt
@@ -5,55 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.zoomato.Adaptar.Notification_adapter
+import com.example.zoomato.databinding.FragmentNotifcationBottomBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Notifcation_BottomFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Notifcation_BottomFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class Notifcation_BottomFragment : BottomSheetDialogFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentNotifcationBottomBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifcation__bottom, container, false)
+        binding = FragmentNotifcationBottomBinding.inflate(layoutInflater, container, false)
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Notifcation_BottomFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Notifcation_BottomFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val notification = listOf(
+            "Your Order has been Cancelled  Successfully",
+            "Order has been picked",
+            "Congrats Your Order Placed"
+        )
+        val notificationimg =
+            listOf(R.drawable.sademoji, R.drawable.truck, R.drawable.congratulation)
+        val adapter = Notification_adapter(ArrayList(notification), ArrayList(notificationimg))
+
+        binding.notfifacitonrc.layoutManager = LinearLayoutManager(requireContext())
+        binding.notfifacitonrc.adapter = adapter
     }
+
+
 }
